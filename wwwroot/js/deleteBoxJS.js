6387758ebad86d435892223c5796eb229893b9b7
Ahmed Hani Abdulatif Kutbi <Ahmed.Kutbi@Student.HTW-Berlin.de>
2023-03-13 01:50:27 +0100

    Adding delete account btn to profile page

---
+++ wwwroot/js/deleteBoxJS.js
@@ -0,0 +1,22 @@
+﻿function showConfirmationBox() {
+	var deleteBtn = document.getElementById('delete-btn');
+	deleteBtn.addEventListener('click', showConfirmationBox);
+	var confirmationBox = document.querySelector('.confirmation-box');
+	var overlay = document.createElement('div');
+	overlay.classList.add('overlay');
+	document.body.appendChild(overlay);
+	setTimeout(function () {
+		confirmationBox.classList.add('show');
+		overlay.classList.add('show');
+	}, 150);
+}
+
+function hideConfirmationBox() {
+	var confirmationBox = document.querySelector('.confirmation-box');
+	var overlay = document.querySelector('.overlay');
+	confirmationBox.classList.remove('show');
+	overlay.classList.remove('show');
+	setTimeout(function () {
+		overlay.remove();
+	}, 300);
+}
